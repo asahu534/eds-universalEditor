a737e2edaa163bb5244ccd11b823223290c747e0
Vignesh Kumar <vigneshx.kumar@mandg.com>
2026-07-14 11:11:33 +0530

    Style Changes

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -5,16 +5,47 @@ import { loadFragment } from '../fragment/fragment.js';
  * loads and decorates the footer
  * @param {Element} block The footer block element
  */
+
 export default async function decorate(block) {
-  // load footer as fragment
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
-
-  // decorate footer DOM
   block.textContent = '';
-  const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
+  const footer = document.createElement('div');
+  footer.classList.add('footer-container');
+
+  const footerNav = document.createElement('div');
+  footerNav.classList.add('footer-nav')
+
+  const footerCopyright = document.createElement('div');
+  footerCopyright.classList.add('footer-copyright');
+
+  const sections = [...fragment.children];
+
+  if (sections[0]) {
+    footerNav.append(sections[0]);
+  }
+  if (sections[1]) {
+    footerCopyright.append(sections[1]);
+  }
+
+  const wrapper = footerNav.querySelector('.default-content-wrapper');
+  if (wrapper) {
+    const children = [...wrapper.children];
+
+    wrapper.innerHTML = '';
+    for (let i = 0; i < children.length; i += 2) {
+      const column = document.createElement('div');
+      column.classList.add('footer-column');
+      column.append(children[i]);
+      if (children[i + 1]) {
+        column.append(children[i + 1]);
+      }
+      wrapper.append(column);
+    }
+  }
+
+  footer.append(footerNav, footerCopyright);
   block.append(footer);
 }
