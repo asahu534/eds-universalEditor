448af32a53f46e1552348c09861ba11890afc948
Vignesh Kumar <vigneshx.kumar@mandg.com>
2026-07-15 09:37:18 +0530

    Header Changes

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -141,7 +141,6 @@ export default async function decorate(block) {
   if (navSections) {
     navSections.querySelectorAll(':scope .default-content-wrapper > ul > li').forEach((navSection) => {
       if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
-      
       navSection.addEventListener('click', () => {
         if (isDesktop.matches) {
           const expanded = navSection.getAttribute('aria-expanded') === 'true';
