5f942aa7c0ab21d1e8e672a61a33f4d52a79ffdf
Pankaj Pal <pankaj.pal@merkle.com>
2026-07-29 18:04:05 +0530

    Add accordion functionality with toggle behavior and icons

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,7 +1,25 @@
-
 export default function decorate(block) {
- [...block.children].forEach((row) => {
-    console.log(row, "row--->");
-});
+  [...block.children].forEach((ele) => {
+    ele.classList.add('accordion-content');
+    const title = ele.children[0];
+    title.classList.add('accordion-title');
+    const plusIcon = document.createElement('img');
+    plusIcon.src = '../../icons/plus.svg';
+    plusIcon.classList.add('accordion-icon');
+    title.appendChild(plusIcon);
+    const content = ele.children[1];
+    content.classList.add('accordion-body');
+    content.style.display = 'none';
+  });
 
+  const titles = block.querySelectorAll('.accordion-title');
+  titles.forEach((title) => {
+    title.addEventListener('click', () => {
+      const content = title.nextElementSibling;
+      const isOpen = content.style.display === 'block';
+      content.style.display = isOpen ? 'none' : 'block';
+      const icon = title.querySelector('.accordion-icon');
+      icon.src = isOpen ? '../../icons/plus.svg' : '../../icons/minus.svg';
+    });
+  });
 }
