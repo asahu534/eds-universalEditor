41fe85df9c7b449d9808950e1b404b93c2b04d6f
Pankaj Pal <pankaj.pal@merkle.com>
2026-07-30 23:12:53 +0530

    Fix showNavigation component type from number to boolean in carousel configuration

---
+++ blocks/carousel/carousel.js
@@ -0,0 +1,16 @@
+export default function decorate(block) {
+
+    const autoplay = block.children[0];
+    autoplay.classList.add('autoplay');
+    const autoplayDelay = block.children[1];
+    autoplayDelay.classList.add('autoplay-delay');
+    const showNavigation = block.children[2];
+    showNavigation.classList.add('show-navigation');
+    const showPagination = block.children[3];
+    showPagination.classList.add('show-pagination');
+    const loop = block.children[4];
+    loop.classList.add('loop');
+    [...block.children].forEach((child) => {
+       console.log(child, 'child-->');
+    });
+}
