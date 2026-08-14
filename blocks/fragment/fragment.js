38d8dd97d20633a20472fe90962c22082a66d3ef
arunima.sahu <arunima.sahu@merkle.com>
2026-08-14 12:58:03 +0530

    fragment model id

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -47,6 +47,7 @@ export async function loadFragment(path) {
 export default async function decorate(block) {
   const link = block.querySelector('a');
   const path = link ? link.getAttribute('href') : block.textContent.trim();
+  console.log(`path >>${path}`);
   const fragment = await loadFragment(path);
   if (fragment) block.replaceChildren(...fragment.childNodes);
 }
