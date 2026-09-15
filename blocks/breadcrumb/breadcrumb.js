a136a554aa9bb709520e16fac4a06999f9129ba6
vkdentsu <145765810+vkdentsu@users.noreply.github.com>
2026-09-15 16:24:46 +0530

    Lint Update

--- blocks/breadcrumb/breadcrumb.js
+++ blocks/breadcrumb/breadcrumb.js
@@ -37,7 +37,7 @@ export default function decorate(block) {
 
     navListParentLink.href = parentPublicPath;
     navListParentLink.textContent = parentTitle;
-    
+
     navListParentItem.append(navListParentLink);
     navList.append(navListParentItem);
   }
